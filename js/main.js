9e67bd0cac828840462c3017e1495b401669a961
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-01-18 11:20:01 +0200

    First test

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var image = new Image();
+  image.src = "sample.jpg";
+  var c = document.getElementById("myCanvas");
+  var ctx = c.getContext("2d");
+  $(image).load(function() {
+    ctx.drawImage(image, 0, 0);
+    var imageData = ctx.getImageData(0, 0, c.width, c.height);
+    var pixels = imageData.data;
+    var numPixels = imageData.width * imageData.height;
+
+    for (var i = 0; i < numPixels; i++) {
+      pixels[i * 4] = 255 - pixels[i * 4]; // Red
+      pixels[i * 4 + 1] = 255 - pixels[i * 4 + 1]; // Green
+      pixels[i * 4 + 2] = 255 - pixels[i * 4 + 2]; // Blue
+    };
+    ctx.clearRect(0, 0, c.width, c.height);
+    imageData.data = pixels;
+    ctx.putImageData(imageData, 0, 0);
+  });
+});
