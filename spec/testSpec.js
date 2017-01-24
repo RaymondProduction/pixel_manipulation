aaa62b45be53cd3f12ad370d6fac6aefc61e83fd
raymondproduction@ukr.net <raymondproduction@ukr.net>
2017-01-24 12:36:17 +0200

    the correct test

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -4,12 +4,39 @@ define(['jquery', 'init', 'load'], function(jQ, init, load) {
 
     it('works for init', function() {
       var picture;
+      // создаем елемент canvas
+      // create element canvas
       var el = jQ(' <canvas id="myCanvas"></canvas>');
-      picture = init.init(el);
+      // кидаем этот виртуальный элемент в DOM
+      // push element canvas to DOM
+      jQ('body').append(el);
+
+      // el has got  array of elements
+      // that's why I use element number 0
+      picture = init.init(el[0]);
       //picture = init.init();
       //picture = load.load(picture,'sample.jpg');
 
-      expect(picture).toEqual('');
+      expect(picture).toEqual(jasmine.any(Object));
+
+      el.remove();
+
+
+    });
+
+      it('works for init 2', function() {
+      var picture;
+      var el = jQ(' <canvas id="myCanvas"></canvas>');
+      jQ('body').append(el);
+      //debugger;
+
+      picture = init.init(el[0]);
+      //picture = init.init();
+      //picture = load.load(picture,'sample.jpg');
+
+      expect(picture).toEqual(jasmine.any(Object));
+
+      el.remove();
 
 
     });
